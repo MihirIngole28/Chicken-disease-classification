bff34a0f731a2558376015772fedc4aeda393652
MihirIngole28 <myingole28@gmail.com>
2023-08-15 13:34:05 -0500

    Stage 3 completed

---
+++ src/cnnClassifier/components/evaluation.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from pathlib import Path
+from src.cnnClassifier.entity.config_entity import EvaluationConfig
+from cnnClassifier.utils.common import save_json
+
+
+class Evaluation:
+    def __init__(self, config: EvaluationConfig):
+        self.config = config
+
+        
